876f108ce741c3a58bd8830cdfe5482c2a7a0ac1
praveen <girish.jaiswal@caratred.com>
2024-05-18 21:09:58 +0530

    first change

--- sampat_industries/sampat_industries/doctype/subcontracting/subcontracting.js
+++ sampat_industries/sampat_industries/doctype/subcontracting/subcontracting.js
@@ -1,8 +1,51 @@
 // Copyright (c) 2024, Niraj and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Subcontracting", {
-// 	refresh(frm) {
+frappe.ui.form.on("Subcontracting", {
+	refresh: function(frm) {
+        console.log("Form Loaded");
+    },
 
-// 	},
-// });
+    get_materials_for_supplier :function(frm){
+        fill_supplier_itmes_table(frm)
+    }
+
+});
+
+function fill_supplier_itmes_table(frm) {
+    if (frm.doc.subcontracted_item?.length > 0) {
+        console.log("Fetching Raw Materials Data");
+        frappe.call({
+            method: 'sampat_industries.sampat_industries.doctype.subcontracting.subcontracting.get_bom_details',
+            args: {
+                data: frm.doc
+            },
+            callback: function (r) {
+                if (r.message) {
+                    update_supplier_child(frm, r.message);
+                } else {
+                    frm.doc.supplied_items = [];
+                    frm.refresh_field('supplied_items');
+                }
+            }
+        });
+    } else {
+        frm.doc.supplied_items = [];
+        frm.refresh_field('supplied_items');
+    }
+}
+
+function update_supplier_child(frm, data) {
+    frm.clear_table('supplied_items');
+    let items = data;
+    for (let index = 0; index < items.length; index++) {
+        let child = frm.add_child('supplied_items');
+        child.main_item_code = items[index]['main_item_code'];
+        child.rm_item_code = items[index]['rm_item_code'];
+        child.bom_detail_no = items[index]['bom_detail_no'];
+        child.reserve_warehouse = items[index]['reserve_warehouse'];
+        child.required_qty = items[index]['required_qty'];
+        child.stock_uom = items[index]['stock_uom'];
+    }
+    frm.refresh_field('supplied_items');
+}
--- sampat_industries/sampat_industries/doctype/subcontracting/subcontracting.py
+++ sampat_industries/sampat_industries/doctype/subcontracting/subcontracting.py
@@ -1,9 +1,43 @@
 # Copyright (c) 2024, Niraj and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+import json
 from frappe.model.document import Document
 
-
 class Subcontracting(Document):
-	pass
+    pass
+
+
+@frappe.whitelist()
+def get_bom_details(data):
+    
+    subcontract_detail = json.loads(data)
+    
+    raw_materials = []
+    
+    for each in subcontract_detail.get("subcontracted_item",[]):
+        
+        bom_doc = frappe.get_cached_doc("BOM", each.get('bom'))
+        bom_item_detail = bom_doc.as_dict()
+        actual_qty = each.get("qty")
+        reserve_warehouse = subcontract_detail.get("set_reserve_warehouse")
+        print(reserve_warehouse,"oooooooooooooooooooo")
+        
+        for each_item in bom_item_detail.get("items"):
+            args = frappe._dict(
+			{
+				"main_item_code": each_item.get("item_code"),
+                "rm_item_code":each_item.get("item_code"),
+                "bom_detail_no":each_item.get("parent"),
+				"reserve_warehouse": reserve_warehouse,
+				"required_qty": each_item.get("qty")*actual_qty,
+                "stock_uom":each_item.get("stock_uom"),
+			}
+            )
+   
+            raw_materials.append(args)
+            
+    return raw_materials
+
+    
