db8e83f218a6b41d87b11d66f1e9aee8b9dd0473
ganureddy <ganureddy54@gmail.com>
2024-05-23 12:14:30 +0530

    added filed in warehaouse in supplier

--- sampat_industries/hooks.py
+++ sampat_industries/hooks.py
@@ -254,7 +254,8 @@ fixtures = [
                     "Stock Entry-custom_address",
                     "Stock Entry-custom_fg_item",
                     "Stock Entry-custom_total_weight",
-                    "Stock Entry-custom_item_name"
+                    "Stock Entry-custom_item_name",
+                    "Supplier-custom_supplier_warehouse"
 
                 },    
             ]]
