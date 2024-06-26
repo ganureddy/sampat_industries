dc67820238a3386fbaebfccd6c52aece360b2a88
ganureddy <ganureddy54@gmail.com>
2024-06-26 10:19:06 +0530

    feat:added auto calculation in stock entry

--- sampat_industries/public/js/doctype_plugin/stock_entry/stock_entry.js
+++ sampat_industries/public/js/doctype_plugin/stock_entry/stock_entry.js
@@ -5,6 +5,21 @@ frappe.ui.form.on("Stock Entry", {
   },
 });
 
+frappe.ui.form.on('Stock Entry', {
+	custom_total_weight(frm) {
+	    let get_item_value = cur_frm.doc.custom_item_value;
+	    console.log(get_item_value,"2345678");
+	    let get_item_qty = cur_frm.doc.custom_total_weight;
+	    let cal_total_amount = get_item_value * get_item_qty;
+	    frm.set_value("custom_total_amount_itc_04", cal_total_amount);
+	},
+	custom_item_value(frm){
+	    let get_item_value = cur_frm.doc.custom_item_value;
+	    let get_item_qty = cur_frm.doc.custom_total_weight;
+	    let cal_total_amount = get_item_value * get_item_qty;
+	    frm.set_value("custom_total_amount_itc_04", cal_total_amount);
+	}
+});
 // frappe.ui.form.on("Stock Entry Detail", {
 //   refresh: function (frm, cdt, cdn) {
 //     frappe.model.set_value(cdt, cdn, "allow_zero_valuation_rate", "1");
